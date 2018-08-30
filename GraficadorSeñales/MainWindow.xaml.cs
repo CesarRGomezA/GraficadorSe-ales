f9a5c695e0bcf27c7ba2a0e68f9b5e56ca479015
Ana Lucia Blanco <analuciablanco@outlook.com>
2018-08-30 10:32:44 -0700

    Se agrego la rampa

--- GraficadorSeñales/MainWindow.xaml.cs
+++ GraficadorSeñales/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace GraficadorSeñales
         {
             InitializeComponent();
 
-            
+
         }
 
         private void BotonGraficar_Click(object sender, RoutedEventArgs e)
@@ -42,18 +42,18 @@ namespace GraficadorSeñales
 
             plnGrafica.Points.Clear();
 
-            for(double i = tiempoInicial; i <= tiempoFinal;  i += periodoMuestreo )
+            for (double i = tiempoInicial; i <= tiempoFinal; i += periodoMuestreo)
             {
                 double valorMuestra = señal.evaluar(i);
 
-                if (Math.Abs (valorMuestra) > señal.AmplitudMaxima)
+                if (Math.Abs(valorMuestra) > señal.AmplitudMaxima)
                 {
                     señal.AmplitudMaxima = Math.Abs(valorMuestra);
                 }
 
                 señal.Muestras.Add(new Muestra(i, valorMuestra));
-                    
-                
+
+
             }
 
             //Recorre una coleccion o arreglo 
@@ -64,8 +64,44 @@ namespace GraficadorSeñales
             }
 
 
-            
-            
+
+
+
+        }
+
+        private void btnGraficarRampa_Click(object sender, RoutedEventArgs e)
+        {
+            double amplitud = double.Parse(txtAmplitud.Text);
+            double fase = double.Parse(txtFase.Text);
+            double frecuencia = double.Parse(txtFrecuencia.Text);
+            double tiempoInicial = double.Parse(txtTiempoInicial.Text);
+            double tiempoFinal = double.Parse(txtTiempoFinal.Text);
+            double frecuenciaMuestreo = double.Parse(txtFrecuenciadeMuestreo.Text);
+
+            SeñalRampa señal = new SeñalRampa();
+
+            double periodoMuestreo = 1 / frecuenciaMuestreo;
+
+            plnGrafica.Points.Clear();
+
+            for (double i = tiempoInicial; i <= tiempoFinal; i += periodoMuestreo)
+            {
+                double valorMuestra = señal.evaluar(i);
+
+                if (Math.Abs(valorMuestra) > señal.AmplitudMaxima)
+                {
+                    señal.AmplitudMaxima = Math.Abs(valorMuestra);
+                }
+
+                señal.Muestras.Add(new Muestra(i, valorMuestra));
+
+            }
+
+            foreach (Muestra muestra in señal.Muestras)
+            {
+                plnGrafica.Points.Add(new Point(muestra.X * scrContenedor.Width, (muestra.Y * ((scrContenedor.Height / 2.0) - 30) * -1) + (scrContenedor.Height / 2)));
+
+            }
 
         }
     }
---
+++ GraficadorSeñales/SeñalRampa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSeñales
+{
+    class SeñalRampa
+    {
+
+        public List<Muestra> Muestras { get; set; }
+        public double AmplitudMaxima { get; set; }
+
+        public SeñalRampa()
+        {
+            
+            AmplitudMaxima = 0.0;
+            Muestras = new List<Muestra>();
+
+        }
+
+        public double evaluar(double tiempo)
+        {
+            double resultado = 0;
+            if (tiempo >= 0)
+            {
+                resultado = tiempo;
+            }
+
+            else
+            { 
+                resultado = 0;
+            }
+
+           
+
+            return resultado;
+        }
+    }
+}
